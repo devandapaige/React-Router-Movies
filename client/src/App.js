922d03bf233de952c53eb838194204dd2a39bc47
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-29 15:25:37 -0400

    inital commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
